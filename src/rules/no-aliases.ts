c2e406be2cab633c3720ae083b8e99b54a6edf1a
Moritz Jacobs <mail@moritzjacobs.de>
2020-01-29 17:17:04 +0100

    fix(rules): improve reporting on no-aliases

--- src/rules/no-aliases.ts
+++ src/rules/no-aliases.ts
@@ -3,7 +3,7 @@ import {DEFAULT_NPM_HOOKS} from "../constants";
 export default {
 	name: "no-aliases",
 	isObjectRule: false,
-	message: "don't alias binaries, use npx/yarn instead",
+	message: "don't alias binaries, use npx/yarn instead ({{name}})",
 	validate: (key: string, value: string) =>
 		key !== value || DEFAULT_NPM_HOOKS.includes(key),
 };
