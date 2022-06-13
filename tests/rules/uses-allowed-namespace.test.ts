8d1b18b38f9032b70fe4627643a9d31fc3ed3d57
Moritz Jacobs <mail@moritzjacobs.de>
2022-06-13 13:05:29 +0200

    feat: additional "script" namespace

--- tests/rules/uses-allowed-namespace.test.ts
+++ tests/rules/uses-allowed-namespace.test.ts
@@ -4,6 +4,7 @@ describe("uses-allowed-namespace.ts", () => {
 	it("should validate correctly", () => {
 		expect(rule.validate("foo")).toBe(false);
 		expect(rule.validate("other:foo")).toBe(true);
+		expect(rule.validate("script:cms")).toBe(true);
 		expect(rule.validate("preFoo")).toBe(false);
 		expect(rule.validate("postFoo")).toBe(false);
 		expect(rule.validate("preother:foo")).toBe(true);
